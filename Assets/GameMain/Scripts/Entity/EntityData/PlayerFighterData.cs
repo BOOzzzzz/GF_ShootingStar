0abe5b75e81d339f342a404a6d1029e05682542f
BOO. <1392555094@qq.com>
2024-09-26 23:48:50 +0800

    fix：调整数据结构

--- Assets/GameMain/Scripts/Entity/EntityData/PlayerFighterData.cs
+++ Assets/GameMain/Scripts/Entity/EntityData/PlayerFighterData.cs
@@ -1,11 +1,28 @@
-﻿using GameFramework.DataTable;
+﻿using System.Collections.Generic;
+using GameFramework.DataTable;
+using UnityEngine;
 
 namespace ShootingStar
 {
     public class PlayerFighterData:EntityData
     {
+        public Dictionary<int,ThrusterData> thrusters = new Dictionary<int,ThrusterData>();
         public PlayerFighterData(int id) : base(id)
         {
+            thrusters[10001] = new ThrusterData(10001, id)
+            {
+                Position = new Vector3(-0.87f, -0.05f, 0)
+            };
+
+            thrusters[10002] = new ThrusterData(10002, id)
+            {
+                Position = new Vector3(-0.75f, -0.154f, 0.242f)
+            };
+
+            thrusters[10003] = new ThrusterData(10003, id)
+            {
+                Position = new Vector3(-0.75f, -0.154f, -0.242f)
+            };
         }
     }
 }
--- Assets/GameMain/Scripts/Entity/EntityData/ThrusterData.cs
+++ Assets/GameMain/Scripts/Entity/EntityData/ThrusterData.cs
@@ -2,6 +2,14 @@
 {
     public class ThrusterData:AccessoryObjectData
     {
+        private float speed;
+
+        public float Speed
+        {
+            get => speed;
+            set => speed = value;
+        }
+
         public ThrusterData(int id, int ownerId) : base(id, ownerId)
         {
         }
